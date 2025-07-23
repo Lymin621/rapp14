403c955088f07840e613c3332958417c6c6c9f09
dswsd <babdoy123@gmail.com>
2025-07-23 11:35:58 +0900

    0723

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import TestPage from './Navigation';
+import SignInPage from './SignInPage';
+import SingUpPage from './SignUpPage';
 
 function App() {
   return (
     <div className="App">
-      
+      <Routes>
+        <Route path={"/"} element={<TestPage />} />
+        <Route path={"/login"} element={<SignInPage />} />
+        <Route path={"/signup"} element={<SingUpPage />} />
+      </Routes>
     </div>
   );
 }
